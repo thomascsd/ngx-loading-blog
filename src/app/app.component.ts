b7db1ede713ac85502cf71349539da4041b0b2a3
thomascsd <thomascsd808@gmail.com>
2020-06-30 23:05:25 +0800

    add ngx-loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from "@angular/core";
+import { INgxLoadingConfig, ngxLoadingAnimationTypes } from "ngx-loading";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  loading = false;
+  config: INgxLoadingConfig = {};
+
+  private setTimeoutInner() {
+    setTimeout(() => (this.loading = false), 1500);
+  }
+
+  onChasingDots() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.chasingDots;
+    this.setTimeoutInner();
+  }
+
+  onCircle() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.circle;
+    this.setTimeoutInner();
+  }
+  onCubeGrid() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.cubeGrid;
+    this.setTimeoutInner();
+  }
+
+  onDoubleBounce() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.doubleBounce;
+    this.setTimeoutInner();
+  }
+
+  onRectangleBounce() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.rectangleBounce;
+    this.setTimeoutInner();
+  }
+
+  onThreeBounce() {
+    this.loading = true;
+    this.config.animationType = ngxLoadingAnimationTypes.threeBounce;
+    this.setTimeoutInner();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgxLoadingModule } from 'ngx-loading';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule , NgxLoadingModule.forRoot({})],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
